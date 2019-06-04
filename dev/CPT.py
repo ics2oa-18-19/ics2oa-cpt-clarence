7cd9193f7298f7bd47a98069e00f4188bbb39113
ClarenceCorpuz <47527535+ClarenceCorpuz@users.noreply.github.com>
2019-06-04 10:41:11 -0400

    Update CPT.py

--- dev/CPT.py
+++ dev/CPT.py
@@ -6,7 +6,7 @@ import arcade
 WIDTH = 1200
 HEIGHT = 670
 
-# Meteors
+# Astroids
 meteor1_x_positions = []
 meteor1_y_positions = []
 
@@ -15,6 +15,7 @@ meteor2_y_positions = []
 
 meteor3_x_positions = []
 meteor3_y_positions = []
+
 
 for _ in range(1):
     x = random.randrange(0, WIDTH)
@@ -34,6 +35,17 @@ for _ in range(4):
     meteor3_x_positions.append(x)
     meteor3_y_positions.append(y)
 
+# Ship
+ship_x_position = WIDTH/2
+ship_y_position = 75
+
+left_pressed = False
+right_pressed = False
+movement = 50
+
+
+
+
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
     arcade.set_background_color(arcade.color.BLACK)
@@ -50,6 +62,17 @@ def setup():
 
 
 def update(delta_time):
+# ship
+    global left_pressed, ship_x_position
+    if left_pressed:
+        ship_x_position -= movement
+
+    global right_pressed, ship_x_position
+    if right_pressed:
+        ship_x_position -= movement
+
+
+# Asteroids
     for index in range(1):
         meteor1_y_positions[index] -= 1
         if meteor1_y_positions[index] < -175:
@@ -68,9 +91,19 @@ def update(delta_time):
             meteor3_y_positions[index] = random.randrange(HEIGHT, HEIGHT + 300)
             meteor3_x_positions[index] = random.randrange(200, 1000)
 
+
+
+
+
+
+
 def on_draw():
     arcade.start_render()
     # Draw in here...
+#Ship
+    draw_ship(ship_x_position, ship_y_position)
+
+# asteroids
     for x, y in zip(meteor1_x_positions, meteor1_y_positions):
         draw_meteor1(x, y)
 
@@ -81,13 +114,19 @@ def on_draw():
         draw_meteor3(x, y)
 
 def on_key_press(key, modifiers):
-    pass
+    global left_pressed, right_pressed
+    if key == arcade.key.D:
+        left_pressed = True
 
 
 def on_key_release(key, modifiers):
-    pass
+    global left_pressed, right_pressed
+    if key == arcade.key.A:
+        left_pressed = False
 
-
+    elif key == arcade.key.D:
+        right_pressed = False
+        
 def on_mouse_press(x, y, button, modifiers):
     pass
 
@@ -95,13 +134,13 @@ def draw_meteor1(x, y):
     arcade.draw_circle_filled(x, y, 125, arcade.color.BROWN_NOSE)
 
 def draw_meteor2(x, y):
-    arcade.draw_circle_filled(x, y, 75, arcade.color.BROWN_NOSE)
+    arcade.draw_circle_filled(x, y, 75, arcade.color.LIGHT_BROWN)
 
 def draw_meteor3(x, y):
-    arcade.draw_circle_filled(x, y, 40, arcade.color.BROWN_NOSE)
+    arcade.draw_circle_filled(x, y, 40, arcade.color.COCOA_BROWN)
 
-    def draw_ship(x, y):
-    arcade.draw_circle_filled(x, y, 50, arcade.color.BLUE)
+def draw_ship(x, y):
+    arcade.draw_circle_filled(ship_x_position, ship_y_position, 50, arcade.color.BLUE)
 
 
 if __name__ == '__main__':
